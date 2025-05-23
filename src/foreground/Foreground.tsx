9bde2888e9531258165920ba2e107a383cc635bc
Julian Kim <kimjulian@grinnell.edu>
2025-05-22 23:00:17 -0400

    Start work on Foreground

--- src/foreground/Foreground.tsx
+++ src/foreground/Foreground.tsx
@@ -1,3 +1,19 @@
+import { GitHub, LinkedIn } from "@mui/icons-material";
+import "./Foreground.css";
+import { IconButton, Stack, Typography } from "@mui/material";
+
 export function Foreground() {
-  return <div className={"foreground"}>Foreground</div>;
+  return (
+    <Stack className={"foreground"} direction={"column"}>
+      <Typography>juliank.im</Typography>
+      <Stack direction={"row"}>
+        <IconButton>
+          <GitHub />
+        </IconButton>
+        <IconButton>
+          <LinkedIn />
+        </IconButton>
+      </Stack>
+    </Stack>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { MainContent } from "./MainContent.tsx";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  colorSchemes: {
+    dark: true,
+  },
+});
 
 createRoot(document.getElementById("root") as Element).render(
   <StrictMode>
-    <MainContent />
+    <ThemeProvider theme={theme}>
+      <MainContent />
+    </ThemeProvider>
   </StrictMode>,
 );
