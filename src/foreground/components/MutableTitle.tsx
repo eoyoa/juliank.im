94dabadfd06980a731a3f9a39a5de605c839c3bd
Julian Kim <kimjulian@grinnell.edu>
2025-05-23 19:21:10 -0400

    Add ref to MutableTitle input

--- src/foreground/components/MutableTitle.tsx
+++ src/foreground/components/MutableTitle.tsx
@@ -1,10 +1,16 @@
 import { InputBase, type InputBaseProps, Typography } from "@mui/material";
-import { type ChangeEvent, useState } from "react";
+import {
+  type ChangeEvent,
+  type ReactNode,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
 const InputBaseWithChildren = ({
   children,
   ...props
-}: InputBaseProps & { children?: React.ReactNode }) => {
+}: InputBaseProps & { children?: ReactNode }) => {
   let value = "";
   if (children) {
     if (typeof children == "string" || typeof children == "number") {
@@ -24,15 +30,26 @@ const InputBaseWithChildren = ({
 
 export function MutableTitle() {
   const [text, setText] = useState<string>("juliank.im");
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const onChange = (value: string) => {
     setText(value);
   };
 
+  useEffect(() => {
+    if (!inputRef.current) return;
+
+    const inputElement = inputRef.current;
+
+    inputElement.focus({ preventScroll: true });
+    inputElement.setSelectionRange(text.length - 1, text.length - 1);
+  }, [text.length]);
+
   return (
     <Typography
       variant={"h1"}
       component={InputBaseWithChildren}
+      inputRef={inputRef}
       textAlign={"center"}
       onChange={(e: ChangeEvent<HTMLInputElement>) => {
         onChange(e.target.value);
