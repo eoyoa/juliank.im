385481a417861fbcc1d78bd6274368c2d2a4055e
Julian Kim <kimjulian@grinnell.edu>
2025-05-23 16:32:02 -0400

    Refactor out MutableTitle.css

--- src/foreground/components/MutableTitle.tsx
+++ src/foreground/components/MutableTitle.tsx
@@ -1,14 +1,17 @@
 import { Typography } from "@mui/material";
-import "./MutableTitle.css";
 
 export function MutableTitle() {
   return (
     <Typography
-      className={"mutable-title"}
       variant={"h1"}
       contentEditable={"plaintext-only"}
       suppressContentEditableWarning={true}
       spellCheck={false}
+      sx={{
+        outlineWidth: 0,
+        outlineStyle: "solid",
+        outlineColor: "transparent",
+      }}
     >
       juliank.im
     </Typography>
