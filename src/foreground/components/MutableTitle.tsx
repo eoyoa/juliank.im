fb8a81e64db1de7ba5c73ffad23d961a2da84c72
Julian Kim <kimjulian@grinnell.edu>
2025-05-22 23:40:28 -0400

    Make MutableTitle mutable

--- src/foreground/components/MutableTitle.tsx
+++ src/foreground/components/MutableTitle.tsx
@@ -1,5 +1,16 @@
 import { Typography } from "@mui/material";
+import "./MutableTitle.css";
 
 export function MutableTitle() {
-  return <Typography variant={"h1"}>juliank.im</Typography>;
+  return (
+    <Typography
+      className={"mutable-title"}
+      variant={"h1"}
+      contentEditable={"plaintext-only"}
+      suppressContentEditableWarning={true}
+      spellCheck={false}
+    >
+      juliank.im
+    </Typography>
+  );
 }
