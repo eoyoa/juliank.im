7c8d7bc8d0453cabfb81ec2dc4b96852df89d97f
Julian Kim <kimjulian@grinnell.edu>
2025-05-22 23:55:18 -0400

    Make LinkButtons actually lead somewhere

--- src/foreground/components/LinkButtons.tsx
+++ src/foreground/components/LinkButtons.tsx
@@ -4,10 +4,12 @@ import { IconButton, Stack } from "@mui/material";
 export function LinkButtons() {
   return (
     <Stack direction={"row"}>
-      <IconButton>
+      <IconButton onClick={() => window.open("https://github.com/eoyoa")}>
         <GitHub />
       </IconButton>
-      <IconButton>
+      <IconButton
+        onClick={() => window.open("https://www.linkedin.com/in/juliank-/")}
+      >
         <LinkedIn />
       </IconButton>
     </Stack>
