03b07d48b64356eef981c36723b329d766911733
Julian Kim <kimjulian@grinnell.edu>
2025-05-23 18:24:36 -0400

    Make MutableTitle use HTMLInputElement as root node
    Closes #5, closes #4

--- src/foreground/components/MutableTitle.tsx
+++ src/foreground/components/MutableTitle.tsx
@@ -1,19 +1,49 @@
-import { Typography } from "@mui/material";
+import { InputBase, type InputBaseProps, Typography } from "@mui/material";
+import { type ChangeEvent, useState } from "react";
+
+const InputBaseWithChildren = ({
+  children,
+  ...props
+}: InputBaseProps & { children?: React.ReactNode }) => {
+  let value = "";
+  if (children) {
+    if (typeof children == "string" || typeof children == "number") {
+      value = children.toString();
+    }
+  }
+
+  return (
+    <InputBase
+      {...props}
+      className={""}
+      value={value}
+      inputProps={{ className: props.className }}
+    />
+  );
+};
 
 export function MutableTitle() {
+  const [text, setText] = useState<string>("juliank.im");
+
+  const onChange = (value: string) => {
+    setText(value);
+  };
+
   return (
     <Typography
       variant={"h1"}
-      contentEditable={"plaintext-only"}
-      suppressContentEditableWarning={true}
-      spellCheck={false}
+      component={InputBaseWithChildren}
+      textAlign={"center"}
+      onChange={(e: ChangeEvent<HTMLInputElement>) => {
+        onChange(e.target.value);
+      }}
       sx={{
         outlineWidth: 0,
         outlineStyle: "solid",
         outlineColor: "transparent",
       }}
     >
-      juliank.im
+      {text}
     </Typography>
   );
 }
