b0e85456eed6e49c4555eeeda4b079c0c02732e0
Julian Kim <kimjulian@grinnell.edu>
2025-05-22 23:32:24 -0400

    Make typography font families easier to edit

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ const theme = createTheme({
     dark: true,
   },
   typography: {
-    fontFamily: `"Roboto Mono", monospace`,
+    fontFamily: ['"Roboto Mono"', "monospace"].join(","),
   },
 });
 
