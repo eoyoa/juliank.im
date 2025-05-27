c8eec6c91b294c4632df9d95be11e013fc677377
Julian Kim <kimjulian@grinnell.edu>
2025-05-26 20:29:14 -0500

    Added basic title mutation
    Closes #7

--- src/foreground/components/mutable-title/MutableTitle.tsx
+++ src/foreground/components/mutable-title/MutableTitle.tsx
@@ -1,20 +1,60 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { EditableTypography } from "./EditableTypography.tsx";
 
-function simulateTextChange(inputElement: HTMLInputElement, text: string) {
+function showCaretAtIndex(inputElement: HTMLInputElement, caretIndex: number) {
   inputElement.focus({ preventScroll: true });
-  inputElement.setSelectionRange(text.length - 1, text.length - 1);
+  inputElement.setSelectionRange(caretIndex, caretIndex);
+}
+
+function getIndexToChange(curr: string, target: string) {
+  let i = 0;
+
+  for (; i < curr.length && i < target.length; i++) {
+    if (curr[i] !== target[i]) {
+      return i;
+    }
+  }
+  return i;
 }
 
 export function MutableTitle() {
-  const [text, setText] = useState<string>("juliank.im");
+  const titles = useMemo(() => ["juliank.im", "i'm juliank."], []);
+
+  const [i, setI] = useState<number>(0);
+  const [text, setText] = useState<string>(titles[i]);
+
   const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
-    if (!inputRef.current) return;
+    const timer = setTimeout(() => {
+      if (i >= titles.length) return;
 
-    simulateTextChange(inputRef.current, text);
-  }, [text]);
+      const targetTitle = titles[i];
+
+      if (text === targetTitle) {
+        const newI = i + 1;
+        console.log("~ changing i", newI);
+        setI(newI);
+      }
+
+      console.log("~ changing text");
+
+      const indexToChange = getIndexToChange(targetTitle, text) + 1;
+      const newTitle =
+        targetTitle.slice(0, indexToChange) + text.slice(indexToChange);
+      console.log("new title", newTitle);
+      setText(newTitle);
+
+      if (inputRef.current) {
+        inputRef.current.value = newTitle;
+        showCaretAtIndex(inputRef.current, indexToChange);
+      }
+    }, 750);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [i, text, titles]);
 
   return (
     <EditableTypography text={text} setText={setText} inputRef={inputRef} />
