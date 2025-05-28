1f79df539d26cb3a929ef5c660cb996666453703
Julian Kim <kimjulian@grinnell.edu>
2025-05-28 13:42:07 -0500

    Remove responsive font sizes
    Preparing for #11

--- src/main.tsx
+++ src/main.tsx
@@ -2,22 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { MainContent } from "./MainContent.tsx";
-import { createTheme, responsiveFontSizes, ThemeProvider } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material";
 import "@fontsource/roboto-mono";
 
-const theme = responsiveFontSizes(
-  createTheme({
-    colorSchemes: {
-      dark: true,
-    },
-    typography: {
-      fontFamily: ['"Roboto Mono"', "monospace"].join(","),
-    },
-  }),
-  {
-    factor: 2.5,
+const theme = createTheme({
+  colorSchemes: {
+    dark: true,
   },
-);
+  typography: {
+    fontFamily: ['"Roboto Mono"', "monospace"].join(","),
+  },
+});
 
 createRoot(document.getElementById("root") as Element).render(
   <StrictMode>
