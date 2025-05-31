37943dabde700cbb3cb3233525171b242a58f704
Julian Kim <kimjulian@grinnell.edu>
2025-05-30 23:19:51 -0500

    Move background WIP message to bottom

--- src/background/Background.tsx
+++ src/background/Background.tsx
@@ -5,12 +5,8 @@ import { Cat } from "./Cat.tsx";
 export function Background() {
   return (
     <Layer>
-      <Stack
-        height={"100%"}
-        direction={"column"}
-        justifyContent={"space-between"}
-      >
-        <Typography>background work in progress...</Typography>
+      <Stack height={"100%"} direction={"column"} justifyContent={"flex-end"}>
+        <Typography align={"right"}>background work in progress...</Typography>
         <Cat />
       </Stack>
     </Layer>
