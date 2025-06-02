6a891686b1e56b1f82b23479f29de91cc172fb76
Julian Kim <kimjulian@grinnell.edu>
2025-06-02 15:00:03 -0500

    Optimize canvas to update on demand

--- src/background/catScene/CatScene.tsx
+++ src/background/catScene/CatScene.tsx
@@ -2,7 +2,7 @@ import { Canvas } from "@react-three/fiber";
 
 export function CatScene() {
   return (
-    <Canvas>
+    <Canvas frameloop={"demand"}>
       <ambientLight intensity={0.1} />
       <directionalLight color="red" position={[0, 0, 5]} />
       <mesh>
