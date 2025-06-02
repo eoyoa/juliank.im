995568ce61645e8de743f4ab86c081cdda415c9e
Julian Kim <kimjulian@grinnell.edu>
2025-06-02 14:30:21 -0500

    Add basic 3D scene and fallback support

--- src/background/Background.tsx
+++ src/background/Background.tsx
@@ -1,10 +1,18 @@
 import { Layer } from "../common/Layer.tsx";
+import { useDetectGPU } from "@react-three/drei";
+import { Suspense } from "react";
 import { Fallback2DScene } from "./fallback/Fallback2DScene.tsx";
+import { CatScene } from "./catScene/CatScene.tsx";
 
 export function Background() {
+  const gpuTier = useDetectGPU();
+  console.debug("gpuTier:", gpuTier);
+
   return (
     <Layer>
-      <Fallback2DScene />
+      <Suspense fallback={null}>
+        {gpuTier.tier <= 1 ? <Fallback2DScene /> : <CatScene />}
+      </Suspense>
     </Layer>
   );
 }
---
+++ src/background/catScene/CatScene.tsx
@@ -0,0 +1,14 @@
+import { Canvas } from "@react-three/fiber";
+
+export function CatScene() {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.1} />
+      <directionalLight color="red" position={[0, 0, 5]} />
+      <mesh>
+        <boxGeometry />
+        <meshBasicMaterial color={"white"} />
+      </mesh>
+    </Canvas>
+  );
+}
